748a393065ac44f9e2b41b974f46a6ff8869473e
mike <michealofobuike@gmail.com>
2024-09-26 09:38:42 +0100

    java update

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -7,13 +7,36 @@ const Signup={
     Password: PassWord.value
 }
 console.log(Signup);
+// function Signin(event){
+//     event.preventDefault()
+//     const verify={
+//         username: Signup.Username.value,
+//         password: Signup.Password.value
+//     }
+//     if (!verify) {
+//         alert('sign up')
+//     } else {
+//         sessionStorage.setItem('string', JSON.stringify(verify))
+//         location.href='../index.html'        
+//     }
+
+// }
 function Signin(event){
     event.preventDefault()
-    const verify={
-        username: Signup.Username.value,
-        password: Signup.Password.value
+    const username = UserName.value.trim();
+    const password = PassWord.value.trim();
+    
+    if (username === '' || password === '') {
+        alert('Please fill in both username and password');
+        return;
     }
+    
+    const verify = {
+        username,
+        password
+    }
+    
     sessionStorage.setItem('string', JSON.stringify(verify))
-    location.href='../index.html'
+    location.href='../index.html'        
 }
 SignUp.addEventListener('submit',Signin)
